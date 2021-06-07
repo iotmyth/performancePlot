51a40a6c5638c6b9751ddefc9e5e794db080f9ec
bloodberrys <36856722+bloodberrys@users.noreply.github.com>
2021-06-07 12:58:02 +0700

    Create iotmyth-plot.m
    master code

---
+++ iotmyth-plot.m
@@ -0,0 +1,59 @@
+format longG
+
+data = readtable('nice.csv', 'ReadVariableNames', false, 'HeaderLines', 2);
+
+% ini untuk format date seperti halnya di jmeter ya
+% x = seconds((datenum(datestr(data.ElapsedTime, 'yyyy-mm-dd hh:MM:ss.fff')) - datenum(datestr(data{1,1}, 'yyyy-mm-dd hh:MM:ss.fff'))) * 100000);
+% x.Format = 'hh:mm:ss';
+
+markers = {'+','*','.','o','x','v','d','^','s','>','<','v','p','h'};
+colors = {'r','b','m','k','y','c','g'};
+lines = {'-','--',':','-.'};
+line_width = 0.9;
+marker_size = 5;
+marker_counter = 1;
+color_counter = 1;
+line_counter = 1;
+ylabels='Response Time (ms)';
+ylabels1='Number of Threads';
+ylabels2='Mega Bytes (MB)';
+ylabels3='TCP Connections';
+legend_base_name = 'data-';
+
+% kalo ini format number dalam menit
+x = (datenum(datestr(data{:,1}, 'yyyy-mm-dd hh:MM:ss.fff')) - datenum(datestr(data{1,1}, 'yyyy-mm-dd hh:MM:ss.fff'))) * 100000/60;
+hold on
+title('Any Plot');
+xlabel('Elapsed time (minutes)','FontSize',12,'FontWeight','bold');
+ylabel(ylabels3,'FontSize',12,'FontWeight','bold');
+
+for i=1:size(data,2)-1
+      plot(x,data{:,i+1},strcat(lines{line_counter},strcat(colors{color_counter},markers{marker_counter})),'MarkerSize',marker_size,'LineWidth',line_width,'DisplayName',strcat(legend_base_name,sprintf('%.0f',i)));
+%  
+% %       disp(i)
+
+      if mod(i,  size(markers,2)) == 0
+         marker_counter = 1;
+      else
+         marker_counter=marker_counter+1;
+      end
+      
+      if mod(i, size(colors,2)) == 0
+          color_counter = 1;
+      else
+          color_counter=color_counter+1;
+      end
+      
+      if mod(i,size(lines,2)) == 0
+         line_counter = 1;
+      else
+         line_counter=line_counter+1;
+      end
+end
+
+
+box on;
+ax = gca;
+ax.YAxis.Exponent = 0;
+legend('show');
+hold off
