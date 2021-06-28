f778e1a4783cd4ff1012f9ba593f6f2e0e1b8c21
bloodberrys <36856722+bloodberrys@users.noreply.github.com>
2021-06-28 07:49:21 +0700

    susahnyaaa

--- pmfjmeter.m
+++ pmfjmeter.m
@@ -26,10 +26,14 @@ y2 = data{:,4};
 
 hold on
 
-% set(gca, 'YScale', 'log')
-stem(x,y);
-stem(x,y1);
-stem(x,y2);
+set(gca, 'YScale', 'log')
+
+p(1) = stem(x,y,'r');
+p(2) = stem(x,y1,'Color',[0 0.7 0]);
+p(3) = stem(x,y2,'b');
+
+% legend(gca,'MQTT Connect','MQTT Disconnect','MQTT Publish')
+
 
 
 
@@ -37,20 +41,37 @@ box on;
 set(gcf,'Units','Inches');
 ax = gca;
 ax.YAxis.Exponent = 0;
+ax.XAxis.Exponent = 0;
 ax.GridLineStyle = ':';
 ax.GridAlpha = 0.3;
 ax.LineWidth = 0.9;
 set(gca,'FontSize',16)
 
 
-title({'Response Times Distribution','Instance Type (m5.xlarge/m5a.xlarge)'},'FontSize',14);
+
+title({'','Response Times Distribution','Instance Type (m5.xlarge/m5a.xlarge)'},'FontSize',14);
 xlabel(ylabels,'FontSize',15);
-ylabel('Number of responses','FontSize',15);
-% ylabel('\boldmath \bf{$\log _{10} (Number\, of\, responses)$}','FontSize',15,'interpreter','latex','FontWeight', 'bold');
+% ylabel('Number of responses','FontSize',15);
+ylabel('\boldmath \bf{$\log _{10} (Number\, of\, responses)$}','FontSize',15,'interpreter','latex','FontWeight', 'bold');
 ax.XAxis.Exponent = 0;
 pos = get(gcf,'Position');
 set(findall(gcf,'-property','FontName'),'FontName','Times New Roman');
 set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)]);
+
+breakxaxis([40000 280000]);
+%https://stackoverflow.com/questions/18117664/how-can-i-show-only-the-legend-in-matlab
+p2(1) = stem(nan,nan,'r','DisplayName','MQTT Connect');
+p2(2) = stem(nan,nan,'DisplayName','MQTT Disconnect','Color',[0 0.7 0]);
+p2(3) = stem(nan,nan,'b','DisplayName','MQTT Publish');
+lgdh=legend(p2);
+lgdh.Location = 'northWest';
+SaveLegendToImage(gca, lgdh, 'testImage', 'tif');
+set(findall(gca, 'type', 'text'), 'visible', 'off')
+
+legend show;
 print(gcf,'-dpdf',strcat('/Users/mymac/Documents/SCRIPTSHEET/SKRIPSI/hasilgrafik/',strcat(dataID,stringCSV)),'-r0');
 savefig(strcat('/Users/mymac/Documents/SCRIPTSHEET/SKRIPSI/hasilgrafik/',strcat(dataID,stringCSV)));
 hold off
+
+
+
